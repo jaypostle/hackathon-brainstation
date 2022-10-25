d81a062d483fd6eb437f59d1e920652584b684e7
Olanrewaju-Ak <akinola.lanrewaju@yahoo.com>
2022-10-25 16:29:09 -0700

    styling done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,158 +1,138 @@
 const cocktailListEl = document.getElementById('cocktail-list');
-const formEl         = document.querySelector('.selection__form');
-
+const formEl = document.querySelector('.selection__form');
 
 // BY COCKTAIL NAME
 // const apiPromise = axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
 
-
-
 let cocktailObj = {
-    title:  '',
-    src:    '',
-    id:     '',
-    ingredients: [],
-    measurements: [],
-    instructions: '',
+  title: '',
+  src: '',
+  id: '',
+  ingredients: [],
+  measurements: [],
+  instructions: '',
 };
 
 // BY INGREDIENT NAME
-const promiseCall = (ingredientFilter) => {
-    const apiPromise = axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientFilter}`);
-    apiPromise.then((response) => {
+const promiseCall = ingredientFilter => {
+  const apiPromise = axios.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientFilter}`
+  );
+  apiPromise.then(response => {
+    randomNumber = randomCocktail(response.data.drinks.length);
+    let newCocktail = response.data.drinks[randomNumber];
+    // cocktailObjArray.push(response.data.drinks[0]);
+    // console.log(cocktailObjArray);
+    cocktailObj.title = newCocktail.strDrink;
+    cocktailObj.id = newCocktail.idDrink;
+    cocktailObj.src = newCocktail.strDrinkThumb;
 
-        
-        randomNumber = randomCocktail(response.data.drinks);
-        let newCocktail = response.data.drinks[randomNumber];
-        // cocktailObjArray.push(response.data.drinks[0]);
-        // console.log(cocktailObjArray);
-        cocktailObj.title = newCocktail.strDrink;
-        cocktailObj.id = newCocktail.idDrink;
-        cocktailObj.src = newCocktail.strDrinkThumb;
-      
-
-        renderCocktail(cocktailObj);
-        
-    })
-}
+    renderCocktail(cocktailObj);
+  });
+};
 
 // let array = ['hello', 'hi', 1, '4', 10];
 
-function randomCocktail(array) {
-    // console.log(array.length);
+function randomCocktail(arrayLength) {
+  // console.log(array.length);
 
-    let randomNumber = Math.floor(Math.random() * array.length);
-    // console.log(randomNumber);
+  let randomNumber = Math.floor(Math.random() * arrayLength);
+  // console.log(randomNumber);
 
-    return randomNumber;
+  return randomNumber;
 }
 
+const renderCocktail = cocktailObj => {
+  const cocktailTitleEl = document.querySelector('.result__title');
+  const cocktailImgEl = document.querySelector('.result__img');
 
+  cocktailTitleEl.innerText = cocktailObj.title;
+  cocktailImgEl.setAttribute('src', cocktailObj.src);
 
-const renderCocktail = (cocktailObj) => {
-    const cocktailTitleEl = document.querySelector('.result__title');
-    const cocktailImgEl = document.querySelector('.result__img');
+  console.log(cocktailObj);
+  console.log('render cocktail was run');
+  // title
+  console.log(cocktailObj.title);
+  // id
+  console.log(cocktailObj.id);
+  // ingredients
+  // instructions
 
-    cocktailTitleEl.innerText = cocktailObj.title;
-    cocktailImgEl.setAttribute('src', cocktailObj.src);
+  ingredientsPromiseCall(cocktailObj.id);
+};
 
-    console.log(cocktailObj);
-    console.log('render cocktail was run');
-    // title
-    console.log(cocktailObj.title);
-    // id
-    console.log(cocktailObj.id);
-    // ingredients
-    // instructions
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
 
-    ingredientsPromiseCall(cocktailObj.id);
-}
+  // cocktailObj = {};
 
-
-
-
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    cocktailObj = {};
-    
-    console.log(e.target.Alcohol.value);
-    let ingredientFilter = e.target.Alcohol.value;
-    console.log(ingredientFilter);
-    promiseCall(ingredientFilter);
-  
-})
-
-
+  console.log(e.target.Alcohol.value);
+  let ingredientFilter = e.target.Alcohol.value;
+  console.log(ingredientFilter);
+  promiseCall(ingredientFilter);
+});
 
 /* PSEUDO CODE */
 // Check what user Selects
-    // event listener on the submit
-// tie each option to a specific variable 
+// event listener on the submit
+// tie each option to a specific variable
 // upon submit, it changes the ingredient variable
 // then creates the promise
 
+const ingredientsPromiseCall = drinkId => {
+  const ingredientsPromise = axios.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
+  );
+  console.log(ingredientsPromise);
+  ingredientsPromise.then(response => {
+    // console.log(response.data.drinks[0].strInstructions);
 
+    let cocktail = response.data.drinks[0];
+    const ingredientsUlEl = document.querySelector('.recipe__ingredients');
+    const measurementsUlEl = document.querySelector('.recipe__measurements');
 
+    const instructionsEl = document.querySelector('.recipe__instructions');
 
+    cocktailObj.instructions = cocktail.strInstructions;
 
+    // Ingredients
+    cocktailObj.ingredients.push(cocktail.strIngredient1);
+    cocktailObj.ingredients.push(cocktail.strIngredient2);
+    cocktailObj.ingredients.push(cocktail.strIngredient3);
+    cocktailObj.ingredients.push(cocktail.strIngredient4);
 
+    // Measurements
+    cocktailObj.measurements.push(cocktail.strMeasure1);
+    cocktailObj.measurements.push(cocktail.strMeasure2);
+    cocktailObj.measurements.push(cocktail.strMeasure3);
 
-const ingredientsPromiseCall = (drinkId) => {
-    const ingredientsPromise = axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`);
-    console.log(ingredientsPromise);
-    ingredientsPromise.then((response) => {
-        // console.log(response.data.drinks[0].strInstructions);
-    
-        let cocktail = response.data.drinks[0];
-        const ingredientsUlEl = document.querySelector('.recipe__ingredients');
-        const measurementsUlEl = document.querySelector('.recipe__measurements');
-    
-        const instructionsEl = document.querySelector('.recipe__instructions');
-    
-        cocktailObj.instructions = cocktail.strInstructions;
-    
-        // Ingredients
-        cocktailObj.ingredients.push(cocktail.strIngredient1);
-        cocktailObj.ingredients.push(cocktail.strIngredient2);
-        cocktailObj.ingredients.push(cocktail.strIngredient3);
-        cocktailObj.ingredients.push(cocktail.strIngredient4);
-    
-        // Measurements
-        cocktailObj.measurements.push(cocktail.strMeasure1);
-        cocktailObj.measurements.push(cocktail.strMeasure2);
-        cocktailObj.measurements.push(cocktail.strMeasure3);
-    
-        console.log(cocktailObj.ingredients, cocktailObj.measurements, cocktailObj.instructions);
-    
-    
-    
-    
-        cocktailObj.ingredients.forEach(item => {
-            const ingredient = document.createElement('li');
-            ingredient.classList.add('recipe__ingredient-item');
-            ingredient.innerText = item;
-    
-            ingredientsUlEl.appendChild(ingredient);
-    
-            
-        })
-    
-        cocktailObj.measurements.forEach(item => {
-            const measurementSpan = document.createElement('li');
-            measurementSpan.classList.add('recipe__measurement-item');
-    
-            measurementSpan.innerText = item;
-            measurementsUlEl.appendChild(measurementSpan);
-        })
-        
-        instructionsEl.innerText = cocktailObj.instructions;
-    
-    })
-}
+    console.log(
+      cocktailObj.ingredients,
+      cocktailObj.measurements,
+      cocktailObj.instructions
+    );
 
+    cocktailObj.ingredients.forEach(item => {
+      const ingredient = document.createElement('li');
+      ingredient.classList.add('recipe__ingredient-item');
+      ingredient.innerText = item;
+      ingredientsUlEl.innerHTML = '';
+      ingredientsUlEl.appendChild(ingredient);
+    });
 
-// All ingredients 
+    cocktailObj.measurements.forEach(item => {
+      const measurementSpan = document.createElement('li');
+      measurementSpan.classList.add('recipe__measurement-item');
+
+      measurementSpan.innerText = item;
+      measurementsUlEl.appendChild(measurementSpan);
+    });
+
+    instructionsEl.innerText = cocktailObj.instructions;
+  });
+};
+
+// All ingredients
 // strIngredient1 strIngredient2 strIngredient3
 
 // Measurements for each alcoholic ingredient
@@ -161,12 +141,10 @@ const ingredientsPromiseCall = (drinkId) => {
 // Instructions
 // strInstructions
 
-
-
 ////// EXTRAS
 
 // Cocktail glass
 // strGlass
 
-// is alcoholic 
+// is alcoholic
 // strAlcoholic
